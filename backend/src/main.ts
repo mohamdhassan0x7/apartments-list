f10a26df62ef322eaeeba6d07747f79ca2e56f2e
mohamdhassan0x7 <mohamedhassan225530@gmail.com>
2025-03-04 23:32:36 +0200

    edit cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3033',
     credentials: true,
   });
   await app.listen(5000, '0.0.0.0');
